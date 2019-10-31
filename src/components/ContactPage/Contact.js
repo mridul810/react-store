738c03988a8fdda54389d1025cf993ce615816cb
Mridul Chatterjee <mridul.chatterjee@motifworks.com>
2019-10-31 14:25:33 +0530

    Dev #comments: React Book store app

---
+++ src/components/ContactPage/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Title from "../Title";
+export default function Contact() {
+  return (
+    <section className="py-5">
+      <div className="row">
+        <div className="col-10 mx-auto col-md-6 my-3">
+          <Title title="contact us" />
+          Contact Us at bookstore@email.com
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/context/productData.js
@@ -0,0 +1,194 @@
+export const items = [
+  {
+    sys: { id: 0 },
+    fields: {
+      title: "The Power of your Subconscious Mind",
+      price: 99,
+      author: "joseph murphy",
+      description:
+        "In this book, 'The power of your subconscious mind', the author fuses his spiritual wisdom and scientific research to bring to light how the sub-conscious mind can be a major influence on our daily lives. Once you understand your subconscious mind, you can also control or get rid of the various phobias that you may have in turn opening a brand new world of positive energy.",
+      featured: true,
+      image: { fields: { file: { url: "img/product-0.jpg" } } },
+      freeShipping: true
+    }
+  },
+  {
+    sys: { id: 1 },
+
+    fields: {
+      title: "Think and Grow Rich",
+      price: 29,
+      author: "Napoleon Hill",
+      description:
+        "Think And Grow Rich has earned itself the reputation of being considered a textbook for actionable techniques that can help one get better at doing anything, not just by rich and wealthy, but also by people doing wonderful work in their respective fields. There are hundreds and thousands of successful people in the world who can vouch for the contents of this book. At the time of author’s death, about 20 million copies had already been sold. Numerous revisions have been made in the book, from time to time, to make the book more readable and comprehensible to the readers.",
+      featured: false,
+
+      image: { fields: { file: { url: "img/product-1.jpg" } } },
+      freeShipping: false
+    }
+  },
+  {
+    sys: { id: 2 },
+
+    fields: {
+      title: "Word Power Made Easy ",
+      price: 30,
+      author: "Norman Lewis",
+      description:
+        "A core book about English language and correct word usage; those who are preparing to sit for CAT, GMAT, GRE, TOEFL and other such examinations would definitely stand to greatly benefit from this book. Anybody who wants to improve one's vocabulary will also find it very useful.",
+      featured: false,
+
+      image: { fields: { file: { url: "img/product-3.jpg" } } },
+      freeShipping: true
+    }
+  },
+  {
+    sys: { id: 3 },
+
+    fields: {
+      title: "The 5 AM Club: Own Your Morning, Elevate Your Life",
+      price: 15,
+      author: "robin sharma",
+      description:
+        "Legendary leadership and elite performance expert Robin Sharma introduced The 5 AM Club concept over twenty years ago, based on a revolutionary morning routine that has helped his clients maximize their productivity, activate their best health and bulletproof their serenity in this age of overwhelming complexity.",
+      featured: false,
+
+      image: { fields: { file: { url: "img/product-4.jpg" } } },
+      freeShipping: false
+    }
+  },
+  {
+    sys: { id: 4 },
+
+    fields: {
+      title: "Life's Amazing Secrets",
+      price: 45,
+      author: "Gaur Gopal Das",
+      description:
+        "While navigating their way through Mumbai's horrendous traffic, Gaur Gopal Das and his wealthy young friend Harry get talking, delving into concepts ranging from the human condition to finding one's purpose in life and the key to lasting happiness.Whether you are looking at strengthening your relationships, discovering your true potential, understanding how to do well at work or even how you can give back to the world, Gaur Gopal Das takes us on an unforgettable journey with his precious insights on these areas of life.",
+      featured: false,
+
+      image: { fields: { file: { url: "img/product-3.jpg" } } },
+      freeShipping: true
+    }
+  },
+  {
+    sys: { id: 5 },
+
+    fields: {
+      title: "Rich Dad Poor Dad",
+      price: 55,
+      author: "Robert T. Kiyosaki ",
+      description:
+        "April 2017 marks 20 years since Robert Kiyosakis Rich Dad Poor Dad first made waves in the Personal Finance arena It has since become the 1 Personal Finance book of all time translated into dozens of languages and sold around the world Rich Dad Poor Dad is Roberts story of growing up with two dads his real father and the father of his best friend his rich dad and the ways in which both men shaped his thoughts about money and investing ",
+      featured: false,
+
+      image: { fields: { file: { url: "img/product-5.jpg" } } },
+      freeShipping: false
+    }
+  },
+
+  {
+    sys: { id: 6 },
+
+    fields: {
+      title: "The Alchemist",
+      price: 198,
+      author: "The Alchemist",
+      description:
+        "Paulo Coelho's enchanting novel has inspired a devoted following around the world. This story, dazzling in its powerful simplicity and inspiring wisdom, is about an Andalusian shepherd boy named Santiago who travels from his homeland in Spain to the Egyptian desert in search of a treasure buried in the Pyramids. Along the way he meets a Gypsy woman, a man who calls himself.",
+      featured: true,
+
+      image: { fields: { file: { url: "img/product-6.jpg" } } },
+      freeShipping: true
+    }
+  },
+  {
+    sys: { id: 7 },
+
+    fields: {
+      title: "How to Win Friends and Influence People",
+      price: 120,
+      author: "Dale Carnegie",
+      description:
+        "This is the book that gave birth to a self-improvement industry that spans the Globe (according to the Daily Express Newspaper). First written in 1936, this is the 2016 edition specially typeset for easy readability and offering terrific value for money. The book has sold over 32 million copies worldwide and has been translated into every major language. Still as vibrant and helpful as the day it was written it offers a simple set of guidelines, lucidly explained, that will enable every reader gain insights into being Popular, Persuasive, Influential and Happy in all of his or her relationships.",
+      featured: false,
+
+      image: { fields: { file: { url: "img/product-7.jpg" } } },
+      freeShipping: false
+    }
+  },
+  {
+    sys: { id: 8 },
+
+    fields: {
+      title: "Stand By Me!",
+      price: 55,
+      author: " Sudeep Nagarkar",
+      description:
+        "On the eve of her twenty-fifth birthday, Ruhi’s family and friends throw her a surprise party. The next morning, she is found lying on the floor of her room, dead. Her brother Kartik is convinced she was murdered. But no one believes him.",
+      featured: false,
+
+      image: { fields: { file: { url: "img/product-8.jpg" } } },
+      freeShipping: true
+    }
+  },
+  {
+    sys: { id: 9 },
+
+    fields: {
+      title: "Attitude Is Everything",
+      price: 35,
+      author: "Jeff Keller ",
+      description:
+        "Do you dread going to work? Do you feel tired, unhappy, weighed down? Have you given up on your dreams? The road to a happier, more successful life starts with your attitude-and your attitude is within your control. Whether your outlook is negative, positive or somewhere in between, Jeff Keller, motivational speaker and coach, will show you how to take control and unleash your hidden potential through three powerful steps: -THINK!",
+      featured: true,
+
+      image: { fields: { file: { url: "img/product-9.jpg" } } },
+      freeShipping: false
+    }
+  },
+  {
+    sys: { id: 10 },
+
+    fields: {
+      title: "General Knowledge ",
+      price: 75,
+      author: "Dr. Binay Karna ",
+      description:
+        "There is so much happening around the world all the time that it becomes difficult for people to keep abreast with all the latest developments around the globe. So, people usually prefer a single source for their general knowledge needs. Lucent’s Objective General Knowledge is one such book that fulfils that requirement. Lucent’s objective General Knowledge is the 3rd edition of the book and was published in the year 2014. This book is of high value to students aspiring for various competitive exams wherein a significant portion of the paper is based on general knowledge. This book aims to eliminate the need for different general knowledge sources and tries to articulate all the important happenings and incidents that have taken place in the world within its 626 pages",
+      featured: false,
+
+      image: { fields: { file: { url: "img/product-10.jpg" } } },
+      freeShipping: true
+    }
+  },
+  {
+    sys: { id: 11 },
+
+    fields: {
+      title: "The Monk Who Sold His Ferrari",
+      price: 110,
+      author: "Robin Sharma",
+      description:
+        "A renowned inspirational fiction, The Monk Who Sold His Ferrari is a revealing story that offers the readers a simple yet profound way to live life. The plot of this story revolves around Julian Mantle, a lawyer who has made his fortune and name in the profession. A sudden heart-attack creates havoc in the successful lawyer’s life. Jolted by the sudden onset of the illness, his practice comes to a standstill. He ponders over material success being worth it all, renounces all of it and leaves for India.",
+      featured: false,
+      image: { fields: { file: { url: "img/product-11.jpg" } } },
+      freeShipping: false
+    }
+  },
+  {
+    sys: { id: 12 },
+
+    fields: {
+      title: "Exam Warriors ",
+      price: 28,
+      author: "Narendra Modi ",
+      description:
+        "Exam Warriors by Narendra Modi is an inspiring book for the youth. Written in a fun and interactive style, with illustrations, activities and yoga exercises, this book will be a friend not only in acing exams but also in facing life. Non-preachy, practical and thought-provoking, Exam Warriors is a handy guide for students in India and across the world.",
+      featured: false,
+      image: { fields: { file: { url: "img/product-12.jpg" } } },
+      freeShipping: true
+    }
+  }
+];
